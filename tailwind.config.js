584f86aca71cd1872c9869d19a5b4908a258edd6
Ishujaat999 <ishudeveloper@gmail.com>
2024-04-03 11:14:49 +0530

    add font family's

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"], // Add index.html to content for PurgeCSS to detect classes in HTML files
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fontOver: ["Font Over", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+      },
+      colors: {
+        dark: "#03031B",
+        primary: "#63C796",
+        firefly: "#0B1426",
+        danger: "#FF2100",
+      },
+      fontSize: {
+        "custom-xl": "120px",
+      },
+    },
   },
   plugins: [],
+  corePlugins: {
+    fontFace: true,
+  },
+  fontFace: {
+    "Font Over": {
+      src: 'url("./assets/fonts/Font Over.otf")',
+    },
+  },
 };

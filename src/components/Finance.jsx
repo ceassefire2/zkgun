7b902ff873e4658e7ecd0e92c02133054c9255ec
Sandeep-Luhaniwal <sandeepluh0001.com>
2024-04-11 09:59:16 +0530

    finance section start

--- src/components/Finance.jsx
+++ src/components/Finance.jsx
@@ -5,20 +5,21 @@ import CommonBtn from './common/CommonBtn'
 const Finance = () => {
     return (
         <>
-            <div className="max-w-[1400px] mx-auto px-4 bg-[#03031b] py-6 sm:py-8 md:py-12 lg:py-16 xl:py-20">
+            <div className="max-w-[1400px] mx-auto px-4 py-6 sm:py-8 md:py-12 lg:py-16 xl:py-20">
                 <div className="flex justify-center items-center gap-2">
                     <ClipboardData />
-                    <p className='font-bold text-sm md:text-base text-white md:leading-[20.8px] font-manrope uppercase'>Fast and secure decentralized finance</p>
+                    <p className='font-bold !leading-[130%] text-sm md:text-base text-white md:leading-[20.8px] font-manrope uppercase'>Fast and secure decentralized finance</p>
+                </div>
+                <div className="flex justify-center my-8">
+                    <CommonBtn btntext="more about us" />
                 </div>
                 <h3 className='font_actay_wide font-medium uppercase !leading-[120%] text-center text-xl sm:text-2xl md:text-3xl xl:text-4xl lg:leading-[46.8px] text-white pt-4'>
                     Swap crypto across chains without a hitch, leveraging our deep liquidity pools from over 20+ sources for instant, vast transactions.
-                    <span className='text-[#9a9aa4]'>
-                        Our ERC-4337 wallet is your ticket to a seamless, gas-free crypto experience, enhanced with RAILGUN for enhanced privacy when swapping your wallets
+                    {" "} <span className='text-[#9a9aa4]'>
+                        Our ERC-4337 wallet is your ticket to a seamless, gas- free crypto experience, integrated with RAILGUN for enhanced privacy when performing transcanctions.
                     </span>
                 </h3>
-                <div className="flex justify-center mt-8">
-                    <CommonBtn btntext="more about us" />
-                </div>
+
             </div>
 
         </>
